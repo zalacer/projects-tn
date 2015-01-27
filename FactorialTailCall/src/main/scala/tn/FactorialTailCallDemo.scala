9b959c801a682789ea9d6b4744ce0383b0ebe054
alacer <tris.nefzger@gmail.com>
2015-01-26 22:03:13 -0500

    Revert "fixed bug"
    This reverts commit 93c9a7fae593bc93303813a287b789c25b3cf308.

--- FactorialTailCall/src/main/scala/tn/FactorialTailCallDemo.scala
+++ FactorialTailCall/src/main/scala/tn/FactorialTailCallDemo.scala
@@ -17,13 +17,12 @@ object FactorialTailCallDemo {
     if (args.length == 0) {Console.err.println("no args");System.exit(0)}
     try {
       for (i <- 0 until args.length) {
-        if (args(i).matches("0+") || !args(i).matches("[0-9]+")) {
-          Console.err.println("argument \"" + args(i) + "\" is not a positive integer...skipping")
-        } else {
+        if (args(i).matches("[0-9]+")) {
           println(factorial(BigInt(args(i))))
+        } else {
+          Console.err.println("argument \"" + args(i) + "\" is not a positive integer...skipping")
         }
-          
-        }
+      }
     } catch {
         case x: Exception => x.printStackTrace    
     }    
