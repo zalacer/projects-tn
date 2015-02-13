cb32e0dd96f9b250b50ef36a9f4bc0f09cb0a0b1
alacer <tris.nefzger@gmail.com>
2015-02-12 19:34:22 -0500

    updated ScalaFunctionsAsArgsDemo

--- ScalaFunctionsAsArgsDemo/src/main/scala/tn/ScalaFunctionsAsArgsDemo.scala
+++ ScalaFunctionsAsArgsDemo/src/main/scala/tn/ScalaFunctionsAsArgsDemo.scala
@@ -2,11 +2,11 @@ package tn
 
 object ScalaFunctionsAsArgsDemo extends App {
  
-def func(i: Int)(a: Int => Int)(b: Int => Int):Int = {
-  a(i) + b(i)
+def func(i: Int)(j: Int)(a: Int => Int)(b: Int => Int):Int = {
+  a(i) + b(j)
 }
 
-val x = func(7)(x => x + 4)(y => y * 3)
+val x = func(7)(5)(x => x + 3)(y => y * 5)
 
 println(x)
 
