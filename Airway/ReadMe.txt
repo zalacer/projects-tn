f29e85fa533c11d276e0259b5f0e4e99e083f775
alacer <tris.nefzger@gmail.com>
2016-04-15 22:48:04 -0400

    Removed Airway

--- Airway/ReadMe.txt
+++
@@ -1,1 +0,0 @@
-Code for Akka Concurrency by Derek Wyatt
--- Airway/src/main/scala/airway/PilotProvider.scala
+++
@@ -1,8 +0,0 @@
-package airway
-
-trait PilotProvider {
-  import akka.actor._
-  def newPilot(plane: ActorRef, autopilot: ActorRef, controls: ActorRef, altimeter: ActorRef): Actor = new Pilot(plane, autopilot, controls, altimeter)
-  def newCoPilot(plane: ActorRef, autopilot: ActorRef, controls: ActorRef, altimeter: ActorRef): Actor = new CoPilot(plane, autopilot, controls, altimeter)
-  def newAutopilot: Actor = new AutoPilot()
-}
